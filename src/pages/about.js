bd0bbaef73d3c58d0a8fa4175352f5c3a9f22043
Aaron Helton <aaron@nytework.com>
2022-03-09 14:38:38 -0500

    Finished Part 2 of Gatsby Tutorial

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const AboutPage = () => {
+    return (
+        <Layout pageTitle='About Me'>
+            <p>Hello! My name is Aaron Helton and I am the creator of this site. I built this site using Gatsby.</p>
+        </Layout>
+    )
+}
+
+export default AboutPage
