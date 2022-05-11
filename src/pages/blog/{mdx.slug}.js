c2d8efaf4b01aef02141edfcdf911dbce7cb05ca
Aaron Helton <aaron@nytework.com>
2022-05-11 01:30:06 -0400

    Complete Gatsby Part 6

---
+++ src/pages/blog/{mdx.slug}.js
@@ -0,0 +1,29 @@
+import Layout from '../../components/layout'
+import { graphql } from 'gatsby'
+import { MDXRenderer } from 'gatsby-plugin-mdx'
+import * as React from 'react'
+
+const BlogPost = ({data}) => {
+    return (
+        <Layout pageTitle="Super Cool Blog Posts">
+            <p>{data.mdx.frontmatter.date}</p>
+            <MDXRenderer>
+                {data.mdx.body}
+            </MDXRenderer>
+        </Layout>
+    )
+}
+
+export const query = graphql`
+    query ($id: String) {
+        mdx(id: {eq: $id}) {
+            frontmatter {
+                title
+                date(formatString: "MMMM D, YYYY")
+            }
+            body
+        }
+    }
+`
+
+export default BlogPost
