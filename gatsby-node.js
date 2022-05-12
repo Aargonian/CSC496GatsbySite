720049a75e816a08106b5f039b1ca61f9969eeb0
Aaron Helton <aaron@nytework.com>
2022-05-12 00:11:18 -0400

    Generate Pokemon Page

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,18 +1,44 @@
-exports.sourceNodes = ({ actions, createNodeId, createContentDigest }) => {
-  const pokemons = [
-    { name: "Pikachu", type: "electric" },
-    { name: "Squirtle", type: "water" },
-  ]
-  pokemons.forEach(pokemon => {
-    const node = {
-      name: pokemon.name,
-      type: pokemon.type,
-      id: createNodeId(`Pokemon-${pokemon.name}`),
-      internal: {
-        type: "Pokemon",
-        contentDigest: createContentDigest(pokemon),
-      },
+const axios = require("axios")
+
+const get = endpoint => axios.get(`https://pokeapi.co/api/v2${endpoint}`)
+
+const getPokemonNames = async (limit) =>
+{
+  data = await get(`/pokemon?limit=${limit}`)
+  names = []
+  data.data.results.forEach((entry) => {
+    names.push(entry.name)
+  })
+  return names
+}
+
+const getPokemonData = names =>
+  Promise.all(
+    names.map(async name => {
+      const { data: pokemon } = await get(`/pokemon/${name}`)
+      return { ...pokemon }
+    })
+  )
+
+exports.createPages = async ({ actions: { createPage } }) => {
+  const allPokemon = []
+  /* For some reason, fetching more than 100 pokemon at once has an inconsistent chance
+     to cause disconnection issues or makes the process take impossibly long*/
+  for(i = 100; i <= 900; i+= 100)
+  {
+    if (i = 900) {
+      i = 898
     }
-    actions.createNode(node)
+    const names = await getPokemonNames(i)
+    tempPokemon = await getPokemonData(names)
+    tempPokemon.forEach((pokemon) => {
+      allPokemon.push(pokemon)
+    })
+  }
+
+  createPage({
+    path: `/pokemon`,
+    component: require.resolve("./src/templates/all-pokemon.js"),
+    context: { allPokemon },
   })
 }
---
+++ src/templates/all-pokemon.js
@@ -0,0 +1,16 @@
+import React from "react"
+import './all-pokemon.css'
+
+export default function AllPokemon({ pageContext: { allPokemon } }) {
+  return (
+    <div>
+        <h1 class="heading">Pokedex</h1>
+        {allPokemon.map(pokemon => (
+            <div key={pokemon.id} class="card">
+                <img src={pokemon.sprites.front_default} alt={pokemon.name} class="card_image"/>
+                <h4 class="card_heading"><strong>{pokemon.name}</strong></h4>
+            </div>
+        ))}
+    </div>
+  )
+}
